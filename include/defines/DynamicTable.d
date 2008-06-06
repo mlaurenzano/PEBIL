be1e9266944dcfd913ea514c1c0d0626baafff00
michaell <michaell@55925d41-9515-0410-9586-c6f35bcc715f>
2008-06-06 19:50:46 +0000

    Forgot to add the defines file for the DynamicTable class in the previous commit...
    git-svn-id: svn+ssh://bono.sdsc.edu/projects/pmac/svnrepos/pmac/trunk/X86ElfInstrumentor@1880 55925d41-9515-0410-9586-c6f35bcc715f

---
+++ include/defines/DynamicTable.d
@@ -0,0 +1,24 @@
+/*
+##################################################################
+ELF32		Elf64
+Offset	Length	Offset	Length	Name		Description
+0       4       0       8       d_tag           Dynamic Array Tags
+--union--
+4       4       8       8       d_val
+4       4       8       8       d_ptr
+--union--
+##################################################################
+*/
+
+#define DYNAMIC_MACROS_BASIS(__str) /** __str **/ \
+        GET_FIELD_BASIS_A(uint64_t,d_val,d_un); \
+        GET_FIELD_BASIS_A(uint64_t,d_ptr,d_un); \
+        GET_FIELD_BASIS(int64_t,d_tag);
+/** END of definitions **/
+
+#define DYNAMIC_MACROS_CLASS(__str) /** __str **/ \
+        GET_FIELD_CLASS_A(uint64_t,d_val,d_un); \
+        GET_FIELD_CLASS_A(uint64_t,d_ptr,d_un); \
+        GET_FIELD_CLASS(int64_t,d_tag);
+/** END of definitions **/
+
