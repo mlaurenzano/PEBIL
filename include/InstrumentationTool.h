98b1a2756f3d8be5bf36e09be4ca2d87cf6316ef
michaell <michaell@55925d41-9515-0410-9586-c6f35bcc715f>
2009-09-25 21:21:38 +0000

    git-svn-id: svn+ssh://bono.sdsc.edu/projects/pmac/svnrepos/pmac/trunk/X86ElfInstrumentor@2928 55925d41-9515-0410-9586-c6f35bcc715f

---
+++ include/InstrumentationTool.h
@@ -0,0 +1,23 @@
+#ifndef _InstrumentationTool_h_
+#define _InstrumentationTool_h_
+
+#include <ElfFileInst.h>
+
+#define FILE_UNK "__FILE_UNK__"
+
+class InstrumentationTool : public ElfFileInst {
+protected:
+    void printStaticFile(Vector<BasicBlock*>* allBlocks, Vector<LineInfo*>* allLineInfos);
+
+public:
+    InstrumentationTool(ElfFile* elf, char* inputFuncList);
+    ~InstrumentationTool() {}
+
+    virtual void declare() { __SHOULD_NOT_ARRIVE; }
+    virtual void instrument() { __SHOULD_NOT_ARRIVE; }
+
+    virtual const char* briefName() { __SHOULD_NOT_ARRIVE; }
+};
+
+
+#endif /* _InstrumentationTool_h_ */
