556f8209e42617c34fe9981a1540eabe4777dfca
michaell <michaell@55925d41-9515-0410-9586-c6f35bcc715f>
2010-05-04 16:42:38 +0000

    Did some testing/slight improvements on MPI/wrapper issues.
    git-svn-id: svn+ssh://bono.sdsc.edu/projects/pmac/svnrepos/pmac/trunk/PEBIL@3303 55925d41-9515-0410-9586-c6f35bcc715f

--- instcode/IOWrappers.c
+++ instcode/IOWrappers.c
@@ -1,7 +1,21 @@
 #include <IOWrappers.h>
 #include <stdarg.h>
 
+#define USING_MPI
+#ifdef USING_MPI
+#include <mpi.h>
+int __wrapper_name(MPI_File_open)(MPI_Comm comm, char* filename, int amode, MPI_Info info, MPI_File *fh){
+    PRINT_INSTR(logfile, "file open -- %x %s %d %x %x", comm, filename, amode, info, fh);
+    return MPI_File_open(comm, filename, amode, info, fh);
+}
+void __wrapper_name(MPI_File_close)(int fh){
+    PRINT_INSTR(logfile, "MPI_File_close");
+    return MPI_File_close(fh);
+}
+#endif // USING_MPI
+
 int __wrapper_name(printf)(const char* format, ...){
+    PRINT_INSTR(stdout, "printf");
     va_list argp;
     va_start(argp, format);
     int retval = __wrapper_name(vfprintf)(stdout, format, argp);
@@ -10,11 +24,13 @@ int __wrapper_name(printf)(const char* format, ...){
 }
 
 int __wrapper_name(vprintf)(const char* format, va_list argp){
+    PRINT_INSTR(stdout, "vprintf");
     int retval = __wrapper_name(vfprintf)(stdout, format, argp);
     return retval;
 }
 
 int __wrapper_name(fprintf)(FILE* stream, const char* format, ...){
+    PRINT_INSTR(stdout, "fprintf");
     va_list argp;
     va_start(argp, format);
     int retval = __wrapper_name(vfprintf)(stream, format, argp);
@@ -23,6 +39,7 @@ int __wrapper_name(fprintf)(FILE* stream, const char* format, ...){
 }
 
 int __wrapper_name(vfprintf)(FILE* stream, const char* format, va_list argp){
+    PRINT_INSTR(stdout, "vfprintf");
     if (stream == NULL){
         PRINT_INSTR(logfile, "fprintf: null file!");
     } else {
