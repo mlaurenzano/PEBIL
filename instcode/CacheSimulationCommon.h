0ce2cdfd51f6498cd88fbcc5470cc0a40dd5924d
jperaza <jperaza@barker.(none)>
2010-06-10 15:03:13 -0700

    refactoring cache simulation code

--- instcode/CacheSimulationCommon.h
+++ instcode/CacheSimulationCommon.h
@@ -1,11 +1,18 @@
-#ifndef _TracerMethods_h_
-#define _TracerMethods_h_
+#ifndef _CacheSim_h_
+#define _CacheSim_h_
 
 #ifdef PER_SET_RECENT
   #define MOSTRECENT(__setIdx) mostRecent[__setIdx]
 #else
   #define MOSTRECENT(__setIdx) mostRecent
 #endif
+
+#define __MAX_LEVEL                      3
+#define __MAX_LINEAR_SEARCH_ASSOC       64
+#define __MAX_STRING_SIZE             1024
+
+#define INVALID_ADDRESS 0xdeadbeef
+
 
 typedef uint32_t     Attribute_t;  
 typedef uint64_t     Counter_t;    
@@ -14,35 +21,6 @@ typedef uint64_t     Counter_t;
 #else
   typedef uint64_t     Address_t;
 #endif
-
-#ifdef NO_SAMPLING_MODE
-  #define __MAXIMUM_BLOCK_VISIT       500000
-#else
-  #define __MAXIMUM_BLOCK_VISIT        50000
-  #ifdef FINE_GRAIN_SAMPLING
-    #define __SAMPLING_INTERVAL_MAX     100000
-    #define __IGNORING_INTERVAL_MAX    1000000
-  #else
-    #define __SAMPLING_INTERVAL_MAX    1000000
-    #define __IGNORING_INTERVAL_MAX   10000000
-  #endif
-
-  #ifdef EXTENDED_SAMPLING
-    Counter_t sampling_interval_max = __SAMPLING_INTERVAL_MAX;
-    Counter_t ignoring_interval_max = __IGNORING_INTERVAL_MAX;
-    #define __WHICH_SAMPLING_VALUE  sampling_interval_max
-    #define __WHICH_IGNORING_VALUE  ignoring_interval_max
-    #define SEGMENT_COUNT 10  /* needs to divide both intervals */
-    Attribute_t rand_value = SEGMENT_COUNT;
-  #else
-    #define __WHICH_SAMPLING_VALUE  __SAMPLING_INTERVAL_MAX
-    #define __WHICH_IGNORING_VALUE  __IGNORING_INTERVAL_MAX
-  #endif
-#endif
-
-#define __MAX_LEVEL                      3
-#define __MAX_LINEAR_SEARCH_ASSOC       64
-#define __MAX_STRING_SIZE             1024
 
 typedef enum {
     repl_lru = 0,
@@ -98,47 +76,5 @@ typedef struct {
     uint8_t   isVictimCacheHierarchy;
 } MemoryHierarchy;
 
-#ifndef NO_SAMPLING_MODE
-typedef enum {
-    sampling_accesses = 0,
-    ignoring_accesses
-} SamplingStatus;
-
-SamplingStatus currentSamplingStatus = ignoring_accesses;
-Counter_t alreadyIgnored = 0;
-Counter_t alreadySampled = 0;
 #endif
 
-typedef struct {
-    Attribute_t blockId;
-    Attribute_t memOpId;
-#ifdef METASIM_32_BIT_LIB
-    Attribute_t unused;
-#endif
-    Address_t   address;
-} BufferEntry;
-
-#define lastFreeIdx blockId
-
-#include <CacheStructures.h>
-
-typedef struct {
-    Counter_t saturationPoint;
-    Counter_t visitCount;
-    Counter_t sampleCount;
-    Counter_t hitMissCounters[__SYSTEM_COUNT*__MAX_LEVEL*Total_AccessStatus];
-} BasicBlockInfo;
-
-typedef struct {
-    Address_t       minAddress;
-    Address_t       maxAddress;
-} DFPatternRange;
-
-typedef struct {
-    BasicBlockInfo* basicBlock;
-    DFPatternType   type;
-    Counter_t       rangeCnt;
-    DFPatternRange* ranges;
-} DFPatternInfo;
-
-#endif /* _TracerMethods_h_ */
