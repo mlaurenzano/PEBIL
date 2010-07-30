63ca4db64ce8848d29c7621fae04cfb9bd0d2c03
Michael Laurenzano <michaell@michaell-laptop.(none)>
2010-07-29 21:47:10 -0700

    Added an io test code.

---
+++ testapps/ioTest.c
@@ -0,0 +1,46 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+#include <syscall.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+typedef void (*simple_func)(void);
+
+#define TEST_NAME "./barbar"
+#define TEST_MSG "foobaronyou"
+
+int main(int argc, char** argv){
+    systest();
+    clibtest();
+}
+
+int clibtest(){
+}
+int systest(){
+    char* buf;
+    struct stat* st = malloc(sizeof(struct stat));
+    int f = open("/dev/null", O_APPEND, O_RDWR);
+    write(f, TEST_MSG, 11);
+    pwrite(f, TEST_MSG, 11, 0);
+    read(f, buf, 0);
+    pread(f, buf, 0, 0);
+    close(f);
+
+    f = creat(TEST_NAME, O_WRONLY);
+    fchmod(f, S_IWOTH | S_IXGRP);
+    fstat(f, st);
+    fprintf(stdout, "stat info: %d %d %d\n", st->st_dev, st->st_uid, st->st_mode);
+    close(f);
+
+    chmod(TEST_NAME, S_IRUSR | S_IWUSR | S_IRGRP);
+
+    stat(TEST_NAME, st);
+    fprintf(stdout, "stat info: %d %d %d\n", st->st_dev, st->st_uid, st->st_mode);
+    free(st);
+
+    return 0;
+}
