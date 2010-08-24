0373d5382cd20ec71f9a3fe1f881f1644cb60fb8
michaell <michaell@trebek.(none)>
2010-08-24 11:27:47 -0700

    Updated license information to be more descriptive.

--- include/defines/X86Instruction.d
+++ include/defines/X86Instruction.d
@@ -1,4 +1,10 @@
-/* This program is free software: you can redistribute it and/or modify
+/* 
+ * This file is part of the pebil project.
+ * 
+ * Copyright (c) 2010, University of California Regents
+ * All rights reserved.
+ * 
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
