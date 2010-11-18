7858f4b9e3e31de26c688ac8c1b43ea25298c175
michaell <michaell@tardis.(none)>
2010-11-18 11:29:36 -0600

    Added some features to readProcessedTrace

--- scripts/readProcessedTrace.py
+++ scripts/readProcessedTrace.py
@@ -4,7 +4,7 @@
 # ./readProcessedTrace.py --application 450.soplex_pds-50.mps --cpu_count 1 --cpu 0 --sysid 77 --level 3 --input_dir SPEC_CPU2006_trace/processed_trace/ | sort -n -k13
 #
 # output format is:
-# hash_code dyn_fp_cnt dyn_mem_cnt L1hr L2hr [L3hr] func_name line_number static_ft_cnt static_mem_cnt avg_memop_size dynamic_bb_cnt
+# hash_code dyn_fp_cnt dyn_mem_cnt L1hr L2hr [L3hr] func_name line_number static_ft_cnt static_mem_cnt avg_memop_size dynamic_bb_cnt dynamic_bb_perc
 
 import getopt
 import string
@@ -12,17 +12,18 @@ import sys
 
 def print_usage(err):
     print 'Error: ' + str(err)
-    print "usage : " + sys.argv[0];
-    print "        --application <application>";
-    print "        --cpu_count <cpu count>";
-    print "        --sysid <sys id>";
-    print "        --level <level count>";
-    print "        --cpu <cpu>";
-    print "        --input_dir <input dir>";
+    print "usage : " + sys.argv[0]
+    print "        --application <application>"
+    print "        --dataset <dataset>"
+    print "        --cpu_count <cpu count>"
+    print "        --sysid <sys id>"
+    print "        --level <level count>"
+    print "        --cpu <cpu>"
+    print "        --input_dir <input dir>"
 
 ## set up command line args                                                                                                                                        
 try:
-    optlist, args = getopt.getopt(sys.argv[1:], '', ['application=', 'cpu_count=', 'sysid=', 'level=', 'cpu=', 'input_dir='])
+    optlist, args = getopt.getopt(sys.argv[1:], '', ['application=', 'dataset=', 'cpu_count=', 'sysid=', 'level=', 'cpu=', 'input_dir='])
 except getopt.GetoptError, err:
     print_usage(err)
     sys.exit(-1)
@@ -32,6 +33,7 @@ if len(args) > 0:
     sys.exit(-1)
 
 application = ''
+dataset = ''
 cpu_count = 0
 sysid = 0
 level = 0
@@ -64,13 +66,19 @@ for i in range(0,len(optlist),1):
             sys.exit(-1)
     elif optlist[i][0] == '--application':
         application = optlist[i][1]
+    elif optlist[i][0] == '--dataset':
+        dataset = optlist[i][1]
     elif optlist[i][0] == '--input_dir':
         input_dir = optlist[i][1]
     else:
         print_usage('unknown argument ' + str(optlist[i][0]))
+        sys.exit(-1)
 
 if application == '':
     print_usage('--application required')
+    sys.exit(-1)
+if dataset == '':
+    print_usage('--dataset required')
     sys.exit(-1)
 if cpu_count == 0:
     print_usage('--cpu_count required')
@@ -88,22 +96,22 @@ if input_dir == '':
     print_usage('--input_dir required')
     sys.exit(-1)
 
-trace_filen = '%(input_dir)s/%(application)s_%(cpu_count)04d_%(cpu)04d.sysid%(sysid)d' % { 'input_dir':input_dir, 'application':application, 'cpu_count':cpu_count, 'cpu':cpu, 'sysid':sysid }
+trace_filen = '%(input_dir)s/%(application)s_%(dataset)s_%(cpu_count)04d_%(cpu)04d.sysid%(sysid)d' % { 'input_dir':input_dir, 'application':application, 'dataset':dataset, 'cpu_count':cpu_count, 'cpu':cpu, 'sysid':sysid }
 tracef = open(trace_filen, 'r')
 trace_data = tracef.readlines()
 tracef.close()
 
-bb2func_filen = '%(input_dir)s/%(application)s_%(cpu_count)04d.bb2func' % { 'input_dir':input_dir, 'application':application, 'cpu_count':cpu_count }
+bb2func_filen = '%(input_dir)s/%(application)s_%(dataset)s_%(cpu_count)04d.bb2func' % { 'input_dir':input_dir, 'application':application, 'dataset':dataset, 'cpu_count':cpu_count }
 bb2funcf = open(bb2func_filen, 'r')
 bb2func_data = bb2funcf.readlines()
 bb2funcf.close()
 
-static_filen = '%(input_dir)s/%(application)s_%(cpu_count)04d.static' % { 'input_dir':input_dir, 'application':application, 'cpu_count':cpu_count }
+static_filen = '%(input_dir)s/%(application)s_%(dataset)s_%(cpu_count)04d.static' % { 'input_dir':input_dir, 'application':application, 'dataset':dataset, 'cpu_count':cpu_count }
 staticf = open(static_filen, 'r')
 static_data = staticf.readlines()
 staticf.close()
 
-bbbytes_filen = '%(input_dir)s/%(application)s_%(cpu_count)04d.bbbytes' % { 'input_dir':input_dir, 'application':application, 'cpu_count':cpu_count }
+bbbytes_filen = '%(input_dir)s/%(application)s_%(dataset)s_%(cpu_count)04d.bbbytes' % { 'input_dir':input_dir, 'application':application, 'dataset':dataset, 'cpu_count':cpu_count }
 bbbytesf = open(bbbytes_filen, 'r')
 bbbytes_data = bbbytesf.readlines()
 bbbytesf.close()
@@ -113,14 +121,16 @@ bb_info = {}
 for i in range(0, len(trace_data), 1):
     toks = [d.strip() for d in trace_data[i].split()]
     if len(toks) != 3 + level:
-        print_usage('invalid number of tokens on line ' + str(i) + ' of trace file ' + str(trace_filen))
+        print_usage('invalid number of tokens on line ' + str(i+1) + ' of trace file ' + str(trace_filen))
+        sys.exit(-1)
     bb_info[toks[0]] = toks[1:]
 
 # find the bb2func info for all of the blocks that appear in bb_info
 for i in range(0, len(bb2func_data), 1):
     toks = [d.strip() for d in bb2func_data[i].split()]
     if len(toks) != 4:
-        print_usage('invalid number of tokens on line ' + str(i) + ' of bb2func file ' + str(bb2func_filen))
+        print_usage('invalid number of tokens on line ' + str(i+1) + ' of bb2func file ' + str(bb2func_filen))
+        sys.exit(-1)
     if bb_info.has_key(toks[0]):
         bb_info[toks[0]].append(toks[1])
         bb_info[toks[0]].append(toks[3])
@@ -129,7 +139,8 @@ for i in range(0, len(bb2func_data), 1):
 for i in range(0, len(static_data), 1):
     toks = [d.strip() for d in static_data[i].split()]
     if len(toks) != 5:
-        print_usage('invalid number of tokens on line ' + str(i) + ' of static file ' + str(static_filen))
+        print_usage('invalid number of tokens on line ' + str(i+1) + ' of static file ' + str(static_filen))
+        sys.exit(-1)
     if bb_info.has_key(toks[0]):
         bb_info[toks[0]].append(toks[1])
         bb_info[toks[0]].append(toks[2])
@@ -139,18 +150,26 @@ for i in range(0, len(static_data), 1):
 for i in range(0, len(bbbytes_data), 1):
     toks = [d.strip() for d in bbbytes_data[i].split()]
     if len(toks) != 2:
-        print_usage('invalid number of tokens on line ' + str(i) + ' of bbbytes file ' + str(bbbytes_filen))
+        print_usage('invalid number of tokens on line ' + str(i+1) + ' of bbbytes file ' + str(bbbytes_filen))
+        sys.exit(-1)
     if bb_info.has_key(toks[0]):
         bb_info[toks[0]].append(toks[1])
 
 # determine number of accesses to each block
+block_total = 0
 for k in bb_info.keys():
     try:
-        bb_info[k].append(str(int(bb_info[k][1]) / int(bb_info[k][6 + level])))
+        block_accesses = int(bb_info[k][1]) / int(bb_info[k][6 + level])
+        bb_info[k].append(str(block_accesses))
+        block_total += block_accesses
     except ValueError, err:
         print_usage(err)
         sys.exit(-1)
 
+
+
 # print each combined block info
 for k in bb_info.keys():
-    print str(k) + ' ' + string.join(bb_info[k])
+    block_perc = float(bb_info[k][8 + level]) / float(block_total)
+    if block_perc > 0.01:
+        print str(k) + ' ' + string.join(bb_info[k]) + ' ' + str(block_perc)
