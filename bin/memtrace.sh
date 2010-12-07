76fb4aa4deb114a76bb1e552e3e4972d40de640e
michaell <michaell@tardis.(none)>
2010-12-06 22:07:09 -0800

    Updated pmactrace to memtrace.

--- bin/memtrace.sh
+++ bin/memtrace.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-#usage="usage: $0 program arg1 arg2 ..."
+#usage="usage: $0 cpu_count program arg1 arg2 ..."
 
-cpu_count=1
+cpu_count="$1"
+shift;
+app="$1"
+shift;
+
 # for MPI runs
 #app_run_prefix="time mpirun -np $cpu_count"
 app_run_prefix="time"
 run_msg="RUNNING ==> "
-
-app="$1"
-shift;
 
 # collect all command line options into args
 args=""
@@ -22,7 +23,7 @@ log_file="$app.tracelog"
 echo "Writing tracing activity log to $app.tracelog"
 echo "" > "$log_file"
 
-run_log_cmd() { echo "$run_msg $1" | tee -a "$log_file" 2>&1; $1 | tee -a "$log_file" 2>&1; }
+run_log_cmd() { echo "$run_msg $1" | tee -a "$log_file" 2>&1; $1 >> "$log_file" 2>&1; }
 
 run_log_cmd "$app_run_prefix $app $args"
 run_log_cmd "pebil --typ jbb --dtl --app $app"
