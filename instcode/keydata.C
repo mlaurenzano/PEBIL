b0ca0e41401b2a2ace7c69ab3888daf92fbc520e
Josh Peraza <jperaza@barker.(none)>
2012-05-28 10:21:41 -0700

    untested implementation of thread key data using get/setspecific

--- instcode/keydata.C
+++ instcode/keydata.C
@@ -8,7 +8,7 @@
 extern "C" {
     void pebil_set_data(pthread_t thread_id, uint64_t image_id, void * data);
     void * pebil_get_data(pthread_t thread_id, uint64_t image_id);
-    void tool_init_thread(pthread_t thread_id);
+    void * tool_init_thread(void *);
     void tool_init_image(uint64_t image_id);
 }
 
@@ -16,21 +16,28 @@ extern "C" {
 //
 void pebil_set_data(pthread_t thread_id, uint64_t image_id, void * data)
 {
-
+    int err = pthread_set_specific(image_id, data);
+    if( err ) {
+        perror("pthread_set_specific:");
+    }
 }
 
 void * pebil_get_data(pthread_t thread_id, uint64_t image_id)
 {
-    return NULL;
+    assert( pthread_self() == thread_id );
+    return pthread_get_specific(image_id);
 }
 
-void tool_init_thread(pthread_t thread_id)
+void tool_image_init(uint64_t * image_id)
 {
-
+    *image_id = 0;
+    pthread_key_create((pthread_key_t*)image_id, NULL);
 }
 
-void tool_init_image(uint64_t image_id)
+void * tool_thread_init(tool_thread_args * args)
 {
-
+    return args->start_function(args->function_args);
 }
+
+
 
